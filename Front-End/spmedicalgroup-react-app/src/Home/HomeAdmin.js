2bfa85dfc4038c40872e22c60bc3d9c8042abbff
Kevin Benevides <39235256801@info.local>
2019-05-13 11:37:12 -0300

    att

--- Front-End/spmedicalgroup-react-app/src/Home/HomeAdmin.js
+++ Front-End/spmedicalgroup-react-app/src/Home/HomeAdmin.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import "./Home.css"
+import login from '../assets/img/icon-login.png'
+import background from '../assets/img/background.jpg'
 class HomeAdmin extends Component {
     render() {
         return (
-            <main>
-                <div>
-                    <nav>
-                        <img src="#" />
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/consultasadmin">Consultas</Link></li>
-                            <li><Link to="/usuarios">Usuarios</Link></li>
-                            <li><Link to="/medicos">Medicos</Link></li>
-                            <li><Link to="/login" style={{color:"red", textDecoration:"none"}}>Deslogar</Link></li>
-                        </ul>
-                    </nav>
-                    <div>
-
+            <div>
+                <header className="header">
+                    <div className=" flexbox wrapper">
+                        <div className="header__image">
+                            <img src={login} alt="Logo do WeWish!" />
+                        </div>
+                        <nav className="header__navbar">
+                            <ul className="header__list">
+                                <li className="header__list-item" style={{borderLeft: "none"}}><Link to="/homeadmin">Home</Link></li>
+                                <li className="header__list-item"><Link to="/consultasadmin">Consultas</Link></li>
+                                <li className="header__list-item"><Link to="/usuarios">Usuarios</Link></li>
+                                <li className="header__list-item"><Link to="/medicos">Medicos</Link></li>
+                                <li className="header__list-item"><Link to="/login" style={{ color: "red", textDecoration: "none" }}>Deslogar</Link></li>
+                            </ul>
+                        </nav>
                     </div>
-                </div>
-            </main>
+
+                </header>
+                <main>
+                    {/* Section com imagem de fundo */}
+                    <section className="main__section" style={{}}>
+                        {/*<img className="main__section-item" src={background}/> */}
+                        <div className="main__section-item">
+                        </div>
+                    </section>
+
+                </main>
+            </div>
         )
     }
 }
