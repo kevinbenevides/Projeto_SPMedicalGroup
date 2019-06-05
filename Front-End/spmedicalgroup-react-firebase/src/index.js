8dada69501a983d4f7a1310abafae032132dd2f3
Kevin Benevides <39235256801@info.local>
2019-06-05 11:28:36 -0300

    Criação do metodo Cadastro

--- Front-End/spmedicalgroup-react-firebase/src/index.js
+++ Front-End/spmedicalgroup-react-firebase/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Localizacao from '../src/pages/Localizacao'
 import * as serviceWorker from './serviceWorker';
 
--- Front-End/spmedicalgroup-react-firebase/src/pages/Localizacao.js
+++ Front-End/spmedicalgroup-react-firebase/src/pages/Localizacao.js
@@ -7,29 +7,36 @@ export default class Localizacao extends Component{
         super();
         this.state = {
             nome: "",
-            Latitude: "",
-            Longitude: "",
             idusuario: "",
+            idade_paciente: "",
+            especialidade_medico: "",
+            latitude: "",
+            longitude: "",
+            doenca_paciente: "",
             listaUsuarios: []
         }
     }
 
-    componentDidMount(){
-        firebase.firestore().collection("Usuarios")
+    // componentDidMount(){
+    //     firebase.firestore().collection("Usuarios")
         
-        .onSnapshot((usuarios) => {
-            let usuariosArray = [];
-            usuarios.forEach((usuario) => {
-                usuariosArray.push({
-                    id: usuario.id,
-                    nome: usuario.data().Nome
-                })
-            })
+    //     .onSnapshot((usuarios) => {
+    //         let usuariosArray = [];
+    //         usuarios.forEach((usuario) => {
+    //             usuariosArray.push({
+    //                 id: usuario.id,
+    //                 nome: usuario.data().Nome
+    //             })
+    //         })
 
-            this.setState({ listaUsuarios : usuariosArray}, () => {
-                console.log(this.state.listaUsuarios)
-            }  )
-        })
+    //         this.setState({ listaUsuarios : usuariosArray}, () => {
+    //             console.log(this.state.listaUsuarios)
+    //         }  )
+    //     })
+    // }
+
+    atualizaEstado(event){
+        this.setState({[event.target.name] : event.target.value});
     }
 
     atualizaEstadoIdUsuario(event){
@@ -38,8 +45,37 @@ export default class Localizacao extends Component{
 
     cadastraLocalizacao(event){
         event.preventDefault()
-        firebase.firestore().collection("Usuarios").add()
-        .then()
+        
+        if(this.state.idusuario !== 0 ){
+            console.log("if")   
+            firebase.firestore().collection("Usuarios").add({
+                nome: this.state.nome,
+                Idade_Paciente: this.state.idade_paciente,
+                Especialidade_Medico: this.state.especialidade_medico,
+                Doenca_Paciente: this.state.doenca_paciente,
+                Localizacao:  this.state.Latitude + "," + this.state.Longitude
+            })
+            .then((resultado) =>{
+                alert("Usuario Cadastrado")
+            }).catch((erro) =>{
+                    console.log('tag', erro)
+                })
+        }else{
+            console.log("eusi")
+            firebase.firestore().collection("Usuarios")
+            .doc(this.state.idusuario)
+            .set({
+                nome: this.state.nome,
+                Idade_Paciente: this.state.idade_paciente,
+                Especialidade_Medico: this.state.especialidade_medico,
+                Doenca_Paciente: this.state.doenca_paciente,
+                Localizacao:  this.state.latitude + "," + this.state.longitude
+            }).then((resultado) =>{
+                alert("Usuario Alterado")
+            }).catch((erro) =>{
+                console.log('tag', erro)
+            })
+        }
         
     }
 
@@ -47,8 +83,8 @@ export default class Localizacao extends Component{
         return(
             <section>
                 <div>
-                    <form>
-                        <p>Selecione um usuário para efetuar o cadastro:</p>
+                    <form onSubmit={this.cadastraLocalizacao.bind(this)}>
+                        {/* <p>Selecione um usuário para efetuar o cadastro:</p>
                         <select value={this.state.idusuario} onChange={this.atualizaEstadoIdUsuario.bind(this)}>
                             <option>Selecione</option>
                             {this.state.listaUsuarios.map((element) => {
@@ -58,14 +94,33 @@ export default class Localizacao extends Component{
                                     </option>
                                 )
                             } )}
-                        </select>
+                        </select> */}
+                        
+                        <p>Insira um Nome para Usuário:</p>
+                        <input name="nome" placeholder="Ex: Kevin" type="text" value={this.state.nome} onChange={this.atualizaEstado.bind(this)} />
+                        
+                        <p>Insira a idade do paciente: </p>
+                        <input name="idade_paciente" placeholder="Ex: 17" type="number" value={this.state.idade_paciente} onChange={this.atualizaEstado.bind(this)}  />
+                        
+                        <p>Insira a Doença do paciente:</p>
+                        <input name="doenca_paciente" placeholder="Ex: Varíola" type="text" value={this.state.doenca_paciente} onChange={this.atualizaEstado.bind(this)}/>
+                        
+                        <p>Insira a Especialidade do Médico:</p>
+                        <input name="especialidade_medico" placeholder="Ex: Psiquiatra" type="text" value={this.state.especialidade_medico} onChange={this.atualizaEstado.bind(this)}/>
+                        
                         <p>Insira aqui a Latitude:</p>
-                        <input name="Latitude" type="geopoint"/>
+                        <input name="Latitude" placeholder="Ex: -23.5345442" type="text" value={this.state.Latitude} onChange={this.atualizaEstado.bind(this)} required/>
+                        
                         <p>Insira aqui a Longitude:</p>
-                        <input name="Longitude" type="geopoint"/>
+                        <input name="Longitude" placeholder="Ex: -46.6493879" type="text" value={this.state.Longitude} onChange={this.atualizaEstado.bind(this)} required/>
+                        
+                        <button type="submit">Enviar</button>
                     </form>
                 </div>
+                <p>-23.5345442</p>
+                <p>-46.6493879</p>
             </section>
+            
         )
     }
 }
